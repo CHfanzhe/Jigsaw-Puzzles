0e311c496f6ce78c2c95688558fdf645b21b029d
凡哲 CH <CHfanzhe@outlook.com>
2026-05-17 09:07:23 +0800

    优化代码

--- JigsawPuzzles/JigsawPuzzles_template.hpp
+++ JigsawPuzzles/JigsawPuzzles_template.hpp
@@ -6,7 +6,7 @@ namespace JigsawPuzzles::Template {
     template<typename NameTag, typename Inherit = void>
     class JigsawPuzzle : public std::conditional_t<std::is_same_v<Inherit, void>, void_herit, void> {
     private:
-        template<typename, typename> friend struct get_name_tag;
+        template<typename, typename> friend struct get_nametag;
         template<typename, typename> friend struct is_JigsawPuzzle;
         template<typename, typename> friend struct has_herit;
         template<typename, typename> friend struct get_herit;
@@ -14,16 +14,20 @@ namespace JigsawPuzzles::Template {
         using InheritArg = std::conditional_t<std::is_same_v<Inherit, void>, void, Inherit>;
     };
 
+//--->  Tool Type
+
     template<typename T, typename = void>
-    struct get_name_tag { using type = void; };
+    struct get_nametag { using type = void; };
     template<typename T>
-    struct get_name_tag<JigsawPuzzle<T>> { using type = T; };
+    struct get_nametag<JigsawPuzzle<T>> { using type = T; };
     template<typename T>
-    struct get_name_tag<T, std::void_t<typename T::Tag>> {
+    struct get_nametag<T, std::void_t<typename T::Tag>> {
         using type = std::conditional_t<std::is_base_of_v<JigsawPuzzle<typename T::Tag>, T>, 
                         typename T::Tag, void>;
     };
-    template<typename T> using get_name_tag_t = typename get_name_tag<T>::type;
+    template<typename T> using get_nametag_t = typename get_nametag<T>::type;
+
+
 
     template<typename T, typename = void>
     struct is_JigsawPuzzle : public std::false_type {};
@@ -42,55 +46,58 @@ namespace JigsawPuzzles::Template {
     std::is_same_v<typename T::InheritArg, void>, std::true_type, std::false_type> {};
     template<typename T> constexpr bool has_herit_v = has_herit<T>::value;
 
+
+
     template<typename T, typename = void> struct get_herit { using type = void; };
     template<typename T> 
     struct get_herit<T, std::enable_if_t<is_JigsawPuzzle_v<T>>> { using type = typename T::InheritArg; };
     template<typename T> using get_herit_t = typename get_herit<T>::type;
 
-
-
-    struct Tuple {
-    public:
-        template<typename Tag, typename... Args>
-        struct Search;
-
-        template<typename Tag, typename T, typename... Args>
-        struct Search<Tag, T, Args...> {
-        private:
-            using other_result = typename Search<Tag, Args...>::type;
-            using this_result = std::conditional_t<is_JigsawPuzzle_v<T>,
-            std::conditional_t<std::is_same_v<get_name_tag_t<T>, Tag>, std::tuple<T>, std::tuple<>>, 
-            std::tuple<>>;
-        public:
-            using type =
-            decltype(std::tuple_cat(std::declval<this_result>(), std::declval<other_result>()));
-        };
-
-        template<typename Tag>
-        struct Search<Tag> { using type = std::tuple<>; };
-    };
+//--->  JigsawPuzzleBoard
 
     template<typename... JigsawPuzzles>
-    class JigsawPuzzle_Board : protected JigsawPuzzles... {
+    class JigsawPuzzleBoard : protected JigsawPuzzles... {
     private:
+        struct Tuple { // Create For Tuple Handle
+        public:
+            template<typename Tag, typename... Args>
+            struct Search;
+
+            template<typename Tag, typename T, typename... Args>
+            struct Search<Tag, T, Args...> {
+            private:
+                using other_result = typename Search<Tag, Args...>::type;
+                using this_result = std::conditional_t<is_JigsawPuzzle_v<T>,
+                std::conditional_t<std::is_same_v<get_nametag_t<T>, Tag>, std::tuple<T>, std::tuple<>>, 
+                std::tuple<>>;
+            public:
+                using type =
+                decltype(std::tuple_cat(std::declval<this_result>(), std::declval<other_result>()));
+            };
+
+            template<typename Tag>
+            struct Search<Tag> { using type = std::tuple<>; };
+        };
+
         static_assert((is_JigsawPuzzle_v<JigsawPuzzles> && ...),
             "All The Args Must Be Base On JigsawPuzzles!");
-        using Args = std::tuple<JigsawPuzzles...>;
     public:
-        template<typename NameTag, typename result = Tuple::Search<NameTag, JigsawPuzzles...>::type>
+    
+        template<typename NameTag, typename result = typename Tuple::Search<NameTag, JigsawPuzzles...>::type>
         constexpr auto Load() noexcept 
             -> typename std::decay_t<std::tuple_element_t<0, result>> & {
-            static_assert((std::tuple_size_v<result> == 1), "Load Faild: Find Zero Or More Same NameTag.");
+            static_assert((std::tuple_size_v<result> == 1),
+                "Load Faild: Find Zero Or More Same NameTag.");
+
             return static_cast<std::decay_t<std::tuple_element_t<0, result>>&>(*this);
         }
-        template<typename NameTag, typename result = Tuple::Search<NameTag, JigsawPuzzles...>::type>
+        template<typename NameTag, typename result = typename Tuple::Search<NameTag, JigsawPuzzles...>::type>
         constexpr auto Load() const noexcept 
             -> const typename std::decay_t<std::tuple_element_t<0, result>> & {
-            static_assert((std::tuple_size_v<result> == 1), "Load Faild: Find Zero Or More Same NameTag.");
+            static_assert((std::tuple_size_v<result> == 1), 
+                "Load Faild: Find Zero Or More Same NameTag.");
+
             return static_cast<const std::decay_t<std::tuple_element_t<0, result>>&>(*this);
-            
         }
     };
-
-
 }
--- main.cpp
+++ main.cpp
@@ -31,11 +31,24 @@ struct PrintV2 : Print {
     }
 };
 
+struct PrintV3 : PrintV2 {
+    inline void PrintFuncln(const std::string& msg) noexcept {
+        std :: cout << msg << std::endl;
+    }
+    inline void PrintFunc(const std::string& msg) noexcept {
+        std :: cout << msg;
+    }
+    inline void PrintFuncHello() noexcept {
+        std :: cout << "Hello, world!" << std :: endl;
+    }
+};
+
 int main(){
-    JigsawPuzzles::Template::JigsawPuzzle_Board<LifeReporter, PrintV2> test;
-    PrintV2& a = test.Load<Tags::PRINT>();
+    JigsawPuzzles::Template::JigsawPuzzleBoard<LifeReporter, PrintV3> test;
+    PrintV3& a = test.Load<Tags::PRINT>();
     a.PrintFunc("Hello, World!!!!!\n");
     a.PrintFuncln("1234567890");
+    a.PrintFuncHello();
 
     std :: cout << "Hello, World!" << std :: endl;
     std :: cin.get();
